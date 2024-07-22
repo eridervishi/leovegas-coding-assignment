32526500a9b6ede191d04f3649ec73fd2f0a2512
Eri Dervishi <eridervishi12@gmail.com>
2024-07-22 03:23:09 +0200

    movie opened class state

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { starMovie, unstarMovie } from '../data/starredSlice'
 import { addToWatchLater, removeFromWatchLater } from '../data/watchLaterSlice'
@@ -8,13 +9,15 @@ const Movie = ({ movie, viewTrailer }) => {
     const isStarred = useSelector(state => state.starred.starredMovies.some(m => m.id === movie.id))
     const isWatchLater = useSelector(state => state.watchLater.watchLaterMovies.some(m => m.id === movie.id))
 
+    const [isOpen, setOpen] = useState(false)
+
     const handleCardClick = (e) => {
-        e.currentTarget.classList.add('opened')
+        setOpen(true)
     }
 
     const handleCloseClick = (e) => {
         e.stopPropagation()
-        e.target.closest('.card').classList.remove('opened')
+        setOpen(false)
     }
 
     const handleStarClick = () => {
@@ -29,28 +32,28 @@ const Movie = ({ movie, viewTrailer }) => {
 
     return (
         <div className="wrapper col-3 col-sm-4 col-md-3 col-lg-3 col-xl-2">
-            <div className="card" onClick={handleCardClick}>
+            <div className={`card ${isOpen ? 'opened' : ''}`} onClick={handleCardClick}>
                 <div className="card-body text-center">
                     <div className="overlay" />
                     <div className="info_panel">
                         <div className="overview">{movie.overview}</div>
                         <div className="year">{movie.release_date?.substring(0, 4)}</div>
-                        <span 
-                            className="btn-star" 
-                            data-testid={isStarred ? "unstar-link" : "starred-link"} 
+                        <span
+                            className="btn-star"
+                            data-testid={isStarred ? "unstar-link" : "starred-link"}
                             onClick={handleStarClick}
                         >
                             <i className={`bi bi-star${isStarred ? '-fill' : ''}`} data-testid={isStarred ? "star-fill" : ""} />
                         </span>
-                        <button 
-                            type="button" 
-                            data-testid={isWatchLater ? "remove-watch-later" : "watch-later"} 
-                            className={`btn btn-light btn-watch-later${isWatchLater ? ' blue' : ''}`} 
+                        <button
+                            type="button"
+                            data-testid={isWatchLater ? "remove-watch-later" : "watch-later"}
+                            className={`btn btn-light btn-watch-later${isWatchLater ? ' blue' : ''}`}
                             onClick={handleWatchLaterClick}
                         >
                             {isWatchLater ? <i className="bi bi-check"></i> : 'Watch Later'}
                         </button>
-                        <button type="button" className="btn btn-dark" onClick={() => viewTrailer(movie)}>View Trailer</button>                                                
+                        <button type="button" className="btn btn-dark" onClick={() => viewTrailer(movie)}>View Trailer</button>
                     </div>
                     <img className="center-block" src={(movie.poster_path) ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : placeholder} alt="Movie poster" />
                 </div>
@@ -59,7 +62,7 @@ const Movie = ({ movie, viewTrailer }) => {
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-        </div>        
+        </div>
     )
 }
 
