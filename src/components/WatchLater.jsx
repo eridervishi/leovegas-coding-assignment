23108ecfe7bd37675acf299a8780c04fc3bc6c56
Eri Dervishi <eridervishi12@gmail.com>
2024-07-22 03:16:55 +0200

    selectors optimisation

--- src/components/WatchLater.jsx
+++ src/components/WatchLater.jsx
@@ -1,42 +1,54 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import watchLaterSlice from '../data/watchLaterSlice'
+import { removeAllWatchLater } from '../data/watchLaterSlice'
 import Movie from './Movie'
 import '../styles/starred.scss'
 
-const WatchLater = ({viewTrailer}) => {
-
-    const state = useSelector((state) => state)
-    const { watchLater } = state
-    const { remveAllWatchLater } = watchLaterSlice.actions
+const WatchLater = ({ viewTrailer }) => {
     const dispatch = useDispatch()
+    const watchLaterMovies = useSelector(state => state.watchLater.watchLaterMovies)
 
-  return (
-    <div className="starred" data-testid="watch-later-div">
-      {watchLater.watchLaterMovies.length > 0 && (<div data-testid="watch-later-movies" className="starred-movies">
-        <h6 className="header">Watch Later List</h6>
-        <div className="row">
-        {watchLater.watchLaterMovies.map((movie) => (
-          <Movie 
-            movie={movie} 
-            key={movie.id}
-            viewTrailer={viewTrailer}
-          />
-        ))}
+    const handleEmptyList = () => {
+        dispatch(removeAllWatchLater())
+    }
+
+    if (watchLaterMovies.length === 0) {
+        return (
+            <div className="starred" data-testid="watch-later-div">
+                <div className="text-center empty-cart">
+                    <i className="bi bi-heart" />
+                    <p>You have no movies saved to watch later.</p>
+                    <p>Go to <Link to='/'>Home</Link></p>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="starred" data-testid="watch-later-div">
+            <div data-testid="watch-later-movies" className="starred-movies">
+                <h6 className="header">Watch Later List</h6>
+                <div className="row">
+                    {watchLaterMovies.map((movie) => (
+                        <Movie 
+                            movie={movie} 
+                            key={movie.id}
+                            viewTrailer={viewTrailer}
+                        />
+                    ))}
+                </div>
+
+                <footer className="text-center">
+                    <button 
+                        className="btn btn-primary" 
+                        onClick={handleEmptyList}
+                    >
+                        Empty list
+                    </button>
+                </footer>
+            </div>
         </div>
-
-        <footer className="text-center">
-          <button className="btn btn-primary" onClick={() => dispatch(remveAllWatchLater())}>Empty list</button>
-        </footer>
-      </div>)}
-
-      {watchLater.watchLaterMovies.length === 0 && (<div className="text-center empty-cart">
-        <i className="bi bi-heart" />
-        <p>You have no movies saved to watch later.</p>
-        <p>Go to <Link to='/'>Home</Link></p>
-      </div>)}
-    </div>
-  )
+    )
 }
 
 export default WatchLater
